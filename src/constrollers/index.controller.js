7a063c26231aa66b82ff981fe2b65988d62a9168
Ernesto Jara <ernesto.jara@nextiva.com>
2019-02-15 23:49:42 -0600

    removing useless messages

--- src/constrollers/index.controller.js
+++ src/constrollers/index.controller.js
@@ -1,7 +1,6 @@
 function HelloWorld (req, res) {
 	return res.status(200).send({
-		message:
-			"welcome to Neto's Company or if your EGO is to high to remove the name of my creator, then fuck you and welcome to where ever the fcuk we are!",
+		message: "welcome to where ever the fcuk we are!",
 	});
 }
 
