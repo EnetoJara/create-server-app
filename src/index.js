26dbe1a5db6df9ed11e3e866fa40920989f66e52
Ernesto Jara <ernesto.jara@nextiva.com>
2019-06-06 10:00:00 -0500

    fiz

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 require("@babel/polyfill");
-require("@babel/register");
-require("@babel/plugin-proposal-async-generator-functions");
-require("../src/server.js");
+require("core-js");
+// eslint-disable-next-line import/no-extraneous-dependencies
+require("regenerator-runtime/runtime");
+require("es6-promise/auto");
+const helpers = require("@babel/helpers");
+const types = require("@babel/types");
+
+const typeofHelper = helpers.get("typeof");
+
+types.isExpressionStatement(typeofHelper);
+require("core-js/modules/es6.promise");
+require("core-js/modules/es6.array.iterator");
+// require("react-hot-loader");
+
+require("./server.js");
